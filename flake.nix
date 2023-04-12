8548427de6f8402754b09591e300cba29f2c794f
Matthew Healy <matthew.healy@tweag.io>
2023-04-12 12:28:54 +0200

    First commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Code for the book Beautiful Racket";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem(system: 
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            racket
+          ];
+        };
+      }
+    );
+}
