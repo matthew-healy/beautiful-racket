89b9758f3c97a0aadf68b2bb39c2c517ffe643be
Matthew Healy <matthew.healy@tweag.io>
2023-04-21 15:41:04 +0200

    Make bf installable

--- bf/reader.rkt
+++ bf/reader.rkt
@@ -4,7 +4,7 @@
 
 (define (read-syntax path port)
 	(define parse-tree (parse path (make-tokenizer port)))
-	(define module-datum `(module bf-mod "expander.rkt"
+	(define module-datum `(module bf-mod bf/expander
 							,parse-tree))
 	(datum->syntax #f module-datum))
 (provide read-syntax)
